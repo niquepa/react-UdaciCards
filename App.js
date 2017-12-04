d7e80d05be5d0df7555022ab3571c39fba09aa19
Carlos Castillo <ccastillo.consultant@dot.nyc.gov>
2017-12-03 23:27:25 -0500

    - AddEntry - Reducer - Actions

--- App.js
+++ App.js
@@ -1,14 +1,63 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import reducer from './reducers';
+import AddDeck from './components/AddDeck';
+import { red } from './utils/colors';
+
+const Tabs = TabNavigator({
+  // History: {
+  //   screen: History,
+  //   navigationOptions: {
+  //     tabBarLabel: 'History',
+  //     tabBarIcon: ({ tintColor }) => <Ionicons name="ios-bookmarks" size={30} color={tintColor} />,
+  //   },
+  // },
+  AddDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'Add Entry',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name="plus-square" size={30} color={tintColor} />,
+    },
+  },
+  // Live: {
+  //   screen: Live,
+  //   navigationOptions: {
+  //     tabBarLabel: 'Live',
+  //     tabBarIcon: ({ tintColor }) => <Ionicons name="ios-speedometer" size={30} color={tintColor} />,
+  //   },
+  // },
+}, {
+  navigationOptions: {
+    header: null,
+  },
+  tabBarOptions: {
+    activeTintColor: red,
+    style: {
+      height: 56,
+      backgroundColor: red,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1,
+    },
+  },
+});
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View style={{ flex: 1 }}>
+          <Tabs />
+        </View>
+      </Provider>
     );
   }
 }
