43ab2be48d404c0a0fb932ca97158c2aa20da633
Carlos Castillo <ccastillo.consultant@dot.nyc.gov>
2017-12-03 23:42:57 -0500

    - DeckDetail - DecksList - Tabs

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native';
 import { TabNavigator, StackNavigator } from 'react-navigation';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import { Constants } from 'expo';
 import reducer from './reducers';
+import DecksList from './components/DecksList';
+import DeckDetail from './components/DeckDetail';
 import AddDeck from './components/AddDeck';
-import { red } from './utils/colors';
+import { red, purple, white } from './utils/colors';
+
+function UdaciStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  );
+}
 
 const Tabs = TabNavigator({
-  // History: {
-  //   screen: History,
-  //   navigationOptions: {
-  //     tabBarLabel: 'History',
-  //     tabBarIcon: ({ tintColor }) => <Ionicons name="ios-bookmarks" size={30} color={tintColor} />,
-  //   },
-  // },
+  DecksList: {
+    screen: DecksList,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <Ionicons name="ios-bookmarks" size={30} color={tintColor} />,
+    },
+  },
   AddDeck: {
     screen: AddDeck,
     navigationOptions: {
@@ -35,10 +46,10 @@ const Tabs = TabNavigator({
     header: null,
   },
   tabBarOptions: {
-    activeTintColor: red,
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
     style: {
       height: 56,
-      backgroundColor: red,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
       shadowColor: 'rgba(0, 0, 0, 0.24)',
       shadowOffset: {
         width: 0,
@@ -50,23 +61,30 @@ const Tabs = TabNavigator({
   },
 });
 
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs,
+  },
+  DeckDetail: {
+    screen: DeckDetail,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple,
+      },
+    },
+  },
+});
+
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
-          <Tabs />
+          <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+          <MainNavigator />
         </View>
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/DeckDetail.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { View, TouchableOpacity, Text, StyleSheet, Platform } from 'react-native';
+import { connect } from 'react-redux';
+// import { NavigationActions } from 'react-navigation';
+
+class DeckDetail extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Detail of Deck</Text>
+      </View>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  // const key = timeToString();
+  //
+  // return {
+  //   alreadyLogged: state[key] && typeof state[key].today === 'undefined',
+  // };
+}
+
+export default DeckDetail;
